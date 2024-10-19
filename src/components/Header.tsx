5ad2be46aa8e8449fc098e277916d904a6d08ee5
Rafael Bueno <78382141+Runderground@users.noreply.github.com>
2024-10-19 19:28:11 +0000

    Modified Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,24 +1,7 @@
-import { useEffect, useRef } from 'react'
-
 export default function Header() {
-  const timer = useRef<HTMLSpanElement>(null)
-
-  useEffect(() => {
-    let tempo: number = 3600
-    if(timer.current) {
-      setInterval(() => {
-        const minutos = Math.floor(tempo / 60)
-        const segundos = tempo % 60;
-
-        tempo--
-
-        timer.current.textContent = `${minutos}:${segundos < 10 ? '0'+segundos : '' +segundos}`
-      }, 1000)
-    }
-  },[])
   return (
     <header style={{width: "100%", textAlign: 'center', background: 'rgba(255, 40 , 50)', fontWeight: '400', letterSpacing: '1px', padding: '.5rem 0'}}>
-      <span style={{color: 'white', fontWeight: 'bold'}}>OFERTA POR POUCO TEMPO!! - <span ref={timer}>60:00</span></span>
+      <span style={{color: 'white', fontWeight: 'bold'}}>OFERTA POR POUCO TEMPO!!</span>
     </header>
   )
 }
