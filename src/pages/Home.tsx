d4a109335c5088bd541d26200be1a4b5cbb4cdc4
Rafael Bueno <78382141+Runderground@users.noreply.github.com>
2024-10-22 00:58:09 +0000

    new: added new link of kiwify

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -27,7 +27,7 @@ export default function Home() {
               funcionalidades desbloqueadas.
             </p>
             <br />
-            <Link to="https://pay.kiwify.com.br/4h3yr1O">Quero Adquirir Agora!</Link>
+            <Link to="https://pay.kiwify.com.br/huJ7rNx">Quero Adquirir Agora!</Link>
           </div>
 
           <img
@@ -78,7 +78,7 @@ export default function Home() {
             />
           </Card.Root>
         </div>
-        <Link to="https://pay.kiwify.com.br/4h3yr1O">Comprar</Link>
+        <Link to="https://pay.kiwify.com.br/huJ7rNx">Comprar</Link>
       </section>
       <section className={style.third_section}>
         <h2>Opiniões de quem já comprou:</h2>
@@ -188,7 +188,7 @@ export default function Home() {
              }
            />
             <h4>R$ 13</h4>
-           <Link to="https://pay.kiwify.com.br/4h3yr1O">Comprar</Link>
+           <Link to="https://pay.kiwify.com.br/huJ7rNx">Comprar</Link>
          </Card.Root>
        </div>
       </section>
